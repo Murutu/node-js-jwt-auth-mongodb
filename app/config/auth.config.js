7b32e9611758e50709a4cb8213c15a7cb854f463
Murutu <murutupeter@gmail.com>
2023-03-23 10:49:39 +0300

    chore: verifySignUp

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "murutu-secret-key"
+}
--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+app.use();
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        userNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
 
 let corsOptions = {
     origin: "http://localhost:8081"
@@ -25,3 +43,49 @@ const PORT  = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 })
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if(!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err)
+                }
+
+                console.log("added 'user' to roles collection")
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err)
+                }
+
+                console.log("added 'user' to roles collection")
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection")
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err)
+                }
+
+                console.log("added 'admin' to roles collection")
+            })
+        }
+    })
+}
